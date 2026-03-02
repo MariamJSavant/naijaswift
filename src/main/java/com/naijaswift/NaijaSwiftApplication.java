c6e3086e800fff708a27e95eab6b4d7f2ecc7e0d
MARIAMJSavant <adewunmimariam2006@gmail.com>
2026-03-02 23:49:52 +0100

    Debugging my Authentication Manager

--- src/main/java/com/naijaswift/NaijaSwiftApplication.java
+++ src/main/java/com/naijaswift/NaijaSwiftApplication.java
@@ -3,7 +3,7 @@ package com.naijaswift;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.naijaswift"})
 public class NaijaSwiftApplication {
     public static void main(String[] args) {
         SpringApplication.run(NaijaSwiftApplication.class, args);
--- src/main/java/com/naijaswift/config/SecurityConfig.java
+++ src/main/java/com/naijaswift/config/SecurityConfig.java
@@ -8,15 +8,39 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.naijaswift.entity.User;
+import com.naijaswift.repository.UserRepository;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+   private final UserRepository userRepository;
 
+   public SecurityConfig(UserRepository userRepository){
+      this.userRepository=userRepository;
+   }
+   @Bean
+   public UserDetailsService userDetailsService(){
+      return email->{
+         User user =userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User with email: "+email+" not found."));
+         return 
+         org.springframework.security.core.userdetails.User.builder()
+            .username(user.getEmail())
+            .password(user.getPassword())
+            .roles("USER")
+            .build();
+
+       
+
+   };
+      }
    @Bean
    public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
@@ -39,13 +63,16 @@ public class SecurityConfig {
    
   
    @Bean
-   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+   public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception{
     http
     .csrf(csrf -> csrf.disable())
      .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/api/users/register").permitAll()
+        .requestMatchers("/api/auth/''","/api/users/register").permitAll()
       .anyRequest().authenticated()
-     );
+     )
+     .sessionManagement(session -> session
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+     ).authenticationProvider(authenticationProvider);
      return http.build();
    }
   
